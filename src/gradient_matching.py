cbe7cd620e024090dbfec05f62433bab8f3413f7
Viljami Raiski <viljami.raiski@aalto.fi>
2020-02-26 18:58:28 +0200

    SECOND_MODEL another 'model added

--- src/gradient_matching.py
+++ src/gradient_matching.py
@@ -2,21 +2,57 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 
+def return_design_matrix(X):
+    return np.concatenate((np.ones(shape=(1, 19)), X))
+
+
 # Copied from the exercise 5
+
+
 def gradient_matching(T, X):
     names = ["SWI5", "CBF1", "GAL4", "GAL80", "ASH1"]
 
     s = "%9s   %11s   %11s   %15s   %18s   %18s   %18s   %18s" % (
         "Component", "Basal level", "Degradation", "Regulation by SWI5", "Regulation by CBF1", "Regulation by GAL4", "Regulation by GAl80", "Regulation by ASH1")
     print(s)
-    constants = np.ones(shape=(5, 19))
-    H = np.concatenate((np.ones(shape=(1, 19)), X[:, 0:-1]))
     for n in range(5):
         dxdt = np.diff(X[n, :]) / np.diff(T)
-        H = np.concatenate((np.ones(shape=(1, 19)), X[:, 0:-1]))
+        H = return_design_matrix(X[:, 0:-1])
         beta = np.linalg.solve(H @ H.T, H @ dxdt)
         regs = np.copy(beta[1:6])
         regs[n] = 0
         s = "{:^9s}   {:10.2f}     {:10.2f}    {:10.2f}            {:^10.2f}         {:^10.2f}           {:^10.2f}       {:^10.2f}".format(
             names[n], beta[0], -beta[n+1], regs[0], regs[1], regs[2], regs[3], regs[4])
         print(s)
+    print("––––––––––––––––––––––––––––––––––––")
+
+
+def gradient_matching_model_2(T, X):
+    names = ["SWI5", "CBF1", "GAL4", "GAL80", "ASH1"]
+    # Interacting genes
+    # SWI5: CBF1, GAL4, GAL80, ASH1
+    indices_SWI5 = [1, 2, 3, 4]
+    # SBF1: SWI5, GAL4, GAL80, ASH1
+    indices_CBF1 = [0, 2, 3, 4]
+    # GAL4: GAL80
+    indices_GAL4 = [3]
+    # GAL80: GAL4, ASH1
+    indices_GAL80 = [2, 4]
+    # ASH1: SWI5, CBF1, GAL4, GAL80
+    indices_ASH1 = [0, 1, 2, 3]
+
+    interaction_indices = [indices_SWI5, indices_CBF1,
+                           indices_GAL4, indices_GAL80, indices_ASH1]
+    for n in range(5):
+        dxdt = np.diff(X[n, :]) / np.diff(T)
+        H = return_design_matrix(np.take(
+            X[:, 0:-1], interaction_indices[n], axis=0))
+        beta = np.linalg.solve(H @ H.T, H @ dxdt)
+        # regs = np.copy(beta[1:6])
+        print("Gene: ", names[n])
+        print("––––––––––––––––––––––––––––––––––––")
+        print("Bias", beta[0])
+        for i, b in enumerate(beta[1:]):
+            print("Effect of {}: {}".format(
+                names[interaction_indices[n][i]], b))
+        print("––––––––––––––––––––––––––––––––––––")
--- src/main.py
+++ src/main.py
@@ -3,6 +3,7 @@ from matplotlib import pyplot as plt
 from data import data
 from utils.utils import plot_result
 from gradient_matching import gradient_matching
+from gradient_matching import gradient_matching_model_2
 
 
 def main():
@@ -10,7 +11,11 @@ def main():
     t_data = data.t_data
     data_legend = ["SWI5", "CBF1", "GAL4", "GAL80", "ASH1"]
     # plot_result(t_data, gene_data, "Gene network", data_legend)
+    # Full model
     gradient_matching(t_data, gene_data)
+
+    # Reduced model
+    gradient_matching_model_2(t_data, gene_data)
 
 
 main()
